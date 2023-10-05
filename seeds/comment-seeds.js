e651deaffb7c86dcea1c19179bce594433dcfc35
Robert Kaiser <paintballmanrob@gmail.com>
2023-10-05 06:20:23 -0400

    Update comment-seeds.js

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,20 +2,30 @@ const { Comment } = require("../models");
 
 const commentData = [
   {
-    comment: "MVC makes creating full-stack apps much cleaner!",
+    comment: "Fly fishing has always fascinated me. Thanks for the tips!",
     user_id: 2,
     post_id: 1,
   },
   {
-    comment: "Sequelize is my favorite ORM!",
-    user_id: 4,
+    comment: "I swear by the Wacky Worm for bass fishing!",
+    user_id: 3,
     post_id: 2,
   },
   {
-    comment: "Great explanation!",
-    user_id: 5,
+    comment: "Deep-sea fishing is not for the faint of heart. Great post!",
+    user_id: 1,
     post_id: 3,
   },
+  {
+    comment: "I totally agree. Your fishing rod choice can make or break your experience.",
+    user_id: 5,
+    post_id: 4,
+  },
+  {
+    comment: "Saltwater fishing is definitely more challenging, but the rewards are worth it!",
+    user_id: 4,
+    post_id: 5,
+  }
 ];
 
 const seedComments = () => Comment.bulkCreate(commentData);
