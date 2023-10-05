c3b9d1c7aba09387c4fb7e4ba0bc010782674c83
Robert Kaiser <paintballmanrob@gmail.com>
2023-10-05 06:20:04 -0400

    Update post-seeds.js

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,23 +2,30 @@ const { Post } = require("../models");
 
 const postData = [
   {
-    title: "Why MVC is so important",
-    content:
-      "MVC allows developers to maintain a true seperation of concerns, devising their code between the Model layer for data, the View layer for design, and the Controller layer for application logic",
+    title: "The Art of Fly Fishing",
+    content: "Fly fishing is a graceful art, requiring finesse and expertise. Here are some tips to get started.",
     user_id: 1,
   },
   {
-    title: "Object-Relational Mapping",
-    content:
-      "I have really loved learning about ORMs. It's really simplified the way I create queries in SQL!",
+    title: "Top 5 Lures for Bass Fishing",
+    content: "When it comes to bass fishing, choosing the right lure is critical. Let's break down the top 5 lures you must have.",
     user_id: 2,
   },
   {
-    title: "Authentication vs. Authorization",
-    content:
-      "There is a difference between authentication and authorization. Authentication means confirming your own identity, whereas authorization means being allowed access to the system",
+    title: "Secrets of Deep Sea Fishing",
+    content: "Venture into the great blue yonder and uncover the secrets of successful deep-sea fishing.",
     user_id: 3,
   },
+  {
+    title: "Choosing the Right Fishing Rod",
+    content: "A guide to selecting the right fishing rod for beginners. Your rod is an extension of your arm!",
+    user_id: 4,
+  },
+  {
+    title: "Freshwater vs Saltwater Fishing",
+    content: "Can't decide between freshwater and saltwater fishing? Let's dive into the pros and cons of both.",
+    user_id: 5,
+  }
 ];
 
 const seedPosts = () => Post.bulkCreate(postData);
